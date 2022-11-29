f02007833c91040c2c0f79c9d8fe6e5d759b6c76
Eric VanBergen <vanberge@gmail.com>
2022-11-29 13:07:16 -0500

    upload redis in main.py too

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ async def websocket_endpoint(websocket: WebSocket, channel_id: str, client_id: i
 
 class RedisService:
     def __init__(self):
-        self.redis_host = f"{os.environ.get('REDIS_HOST', 'redis://localhost')}"
+        self.redis_host = f"{os.environ.get('REDIS_HOST', 'redis://10.143.3.243:6379')}"
 
     async def get_conn(self):
         return await aioredis.from_url(self.redis_host, encoding="utf-8", decode_responses=True)
